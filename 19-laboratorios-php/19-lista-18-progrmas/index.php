625ec1d92102d7010c8daae1472256e8eb6b0ae1
aldehir <111530898+aldehir07@users.noreply.github.com>
2025-01-03 03:17:14 -0500

    lista que muestra los 19 programas

---
+++ 19-laboratorios-php/19-lista-18-progrmas/index.php
@@ -0,0 +1,68 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <title>Title</title>
+</head>
+<body>
+    <div class="container mt-5">
+        <h1 class="text-center">Problemas Desarrollados</h1>
+        <ul class="list-group">
+            <li class="list-group-item">
+                <a href="http://localhost/laboratorio3/1/index.php">Suma de dos numeros florantes</a>
+            </li>
+            <li class="list-group-item">
+                <a href="http://localhost/laboratorio3/2/index.php">Calcular la suma, resta, multiplicacion y division de dos numeros</a>
+            </li>
+            <li class="list-group-item">
+                <a href="http://localhost/laboratorio3/3/index.php">Calcular el area de un circulo</a>
+            </li>
+            <li class="list-group-item">
+                <a href="http://localhost/laboratorio3/4/indexSalario.php">Calcular Salario neto de un trabajador</a>
+            </li>
+            <li class="list-group-item">
+                <a href="http://localhost/laboratorio3/5/index.php">Calcular calificacion de un alumno</a>
+            </li>
+            <li class="list-group-item">
+                <a href="http://localhost/laboratorio3/6/index.php">Calcular Calificación Final en Algoritmos</a>
+            </li>
+            <li class="list-group-item">
+                <a href="http://localhost/laboratorio3/7/index.php">Distribuir Presupuesto Anual de un Hospital</a>
+            </li>
+            <li class="list-group-item">
+                <a href="http://localhost/laboratorio3/8/index.php">Calcular Porcentaje de Inversión en una Empresa</a>
+            </li>
+            <li class="list-group-item">
+                <a href="http://localhost/laboratorio3/9/index.php">Calcular Porcentaje de Alumnos y Alumnas</a>
+            </li>
+            <li class="list-group-item">
+                <a href="http://localhost/laboratorio3/10/index.php">Calcular Costo de un Proceso por Tiempo</a>
+            </li>
+            <li class="list-group-item">
+                <a href="http://localhost/laboratorio3/11/index.php">Convertir de Centímetros a Pulgadas</a>
+            </li>
+            <li class="list-group-item">
+                <a href="http://localhost/laboratorio3/12/index.php">Convertir de Libras a Kilogramos</a>
+            </li>
+            <li class="list-group-item">
+                <a href="http://localhost/laboratorio3/13/index.php">Imprimir Triángulo de Asteriscos</a>
+            </li>
+            <li class="list-group-item">
+                <a href="http://localhost/laboratorio3/14/index.php">Tabla de Multiplicar con Vínculo</a>
+            </li>
+            <li class="list-group-item">
+                <a href="http://localhost/laboratorio3/15/index.php">Números Cifrados con MD5</a>
+            </li>
+            <li class="list-group-item">
+                <a href="http://localhost/laboratorio3/16/index.php">Números Cifrados con MD5 y Modal</a>
+            </li>
+            <li class="list-group-item">
+                <a href="http://localhost/laboratorio3/17/index.php">Valores Aleatorios con Progress Bar y Colores</a>
+            </li>
+        </ul>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+</body>
+</html>
